4db7e0942d45c5e77e31b79d0b2ce5f23488aa1c
Niklas Röttger <niklas.roettger@outlook.de>
2024-11-04 14:33:56 +0100

    add session code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,49 @@
 console.clear();
+
+// Function declaration
+
+/*
+
+function printPersonalData() {
+    const name = "Alex";
+    const age = 24;
+    console.log(name + " is " + age + " years old");
+}
+
+printPersonalData();
+
+*/
+
+// Function parameters
+
+/* 
+
+function printPersonalData(name, age) {
+  console.log(name + " is " + age + " years old");
+}
+
+printPersonalData("Lars", 23);
+
+printPersonalData("Jordan", 25);
+
+*/
+
+// Return statement
+
+function calculateSum(a, b) {
+  return a + b;
+}
+
+const result = calculateSum(3, 4);
+console.log(result);
+
+function checkInputLength(inputString) {
+  if (inputString.length > 3) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const isValid = checkInputLength("Hey");
+console.log(isValid);
